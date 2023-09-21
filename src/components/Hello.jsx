aca5b6b6011ccb2d957a6d6a37efb652e309db9a
Axel Ivan Santiago <axelivanlux@gmail.com>
2023-09-21 17:32:25 -0400

    update

--- src/components/Hello.jsx
+++ src/components/Hello.jsx
@@ -1,15 +1,32 @@
 import React from "react";
 import "../styles/Hello.css";
 import ROCK1 from "../images/rock.svg";
+import LAND from "../images/land.svg";
+import CLOUD from "../images/cloud.svg";
+import CLOUD2 from "../images/cloud2.svg";
+import LAND1 from "../images/land1.svg";
+import TUBE from "../images/tube.svg";
+import ME1 from "../images/me1.png";
+import LeviRock from "../images/lavitateRock.svg";
+import COMP from "../images/computer.svg";
 
 function Hello() {
   return (
     <div className="hello__container">
+      {" "}
+      <img id="rock1" src={ROCK1} alt="icon" />
+      <img id="land" src={LAND} alt="icon" width={150} />
+      <img id="cloud" src={CLOUD} alt="icon" width={350} />
+      <img id="land1" src={LAND1} alt="icon" width={200} />
+      <img id="tube" src={TUBE} alt="icon" width={50} />
+      <img id="me1" src={ME1} alt="icon" width={80} />
+      <img id="levitateRock" src={LeviRock} alt="icon" width={420} />
+      <img id="cloud2" src={CLOUD2} alt="icon" width={250} />
+      <img id="comp" src={COMP} alt="icon" width={430} />
       <div className="hello__left-bar">
         <div className="hello__text__container">
           <p className="hello__heading-3">Hi all. I am</p>
           <div className="typewriter">
-            <img id="rock1" src={ROCK1} alt="icon" />
             <h1>Axel Santiago</h1>
             <h2 className="skill">
               <span id="skill">{">"}</span> Front End React Developer
